46f9db6c16536c8d1d582dcc7cebdcfc808050eb
Thomas Biesaart <thomas@infi.nl>
2020-10-21 21:30:49 +0200

    Generate type declarations

--- rollup.config.js
+++ rollup.config.js
@@ -14,8 +14,8 @@ console.log(name);
 export default {
     input: 'src/index.ts',
     output: [
-        { file: pkg.module, format: 'es' },
-        { file: pkg.main, format: 'umd', name }
+        {file: pkg.module, format: 'es'},
+        {file: pkg.main, format: 'umd', name}
     ],
     plugins: [
         svelte({
